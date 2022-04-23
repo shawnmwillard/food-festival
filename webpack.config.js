088e7e2e7c604c46e716089945097d4bcc5c5511
Shawn Willard <shawnmwillard@gmail.com>
2022-04-23 10:57:50 -0500

    aligned with module source code

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
-const path = require("path");
 const webpack = require("webpack");
+const path = require("path");
+
 module.exports = {
   entry: "./assets/js/script.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.join(__dirname + "/dist"),
     filename: "main.bundle.js",
   },
   plugins: [
